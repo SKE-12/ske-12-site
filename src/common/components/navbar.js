0c9e55e94599d60a1110b4c1add646a21ae6784c
Arut Thanomwatana <arut.tha@gmail.com>
2018-10-01 10:01:50 +0700

    Add login navbar

--- src/common/components/navbar.js
+++ src/common/components/navbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Menu } from 'element-react'
+import '../styles/navbar.css'
 import 'element-theme-default'
 export default class Navbar extends React.Component {
 	changePage (page) {
@@ -12,6 +13,7 @@ export default class Navbar extends React.Component {
 				<Menu theme='dark' defaultActive='1' className='el-menu-demo' mode='horizontal' onSelect={this.changePage.bind(this)}>
 					<Menu.Item index='1'>List</Menu.Item>
 					<Menu.Item index='2'>Map</Menu.Item>
+					<Menu.Item className='PullRight' index='3'>Login</Menu.Item>
 				</Menu>
 			</div>
 		)
