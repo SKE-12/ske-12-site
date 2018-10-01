ca11ee5eccafe1dbf3dc2df82ec550118d5fac60
Arut Thanomwatana <arut.tha@gmail.com>
2018-10-01 10:46:19 +0700

    Add login form

--- src/common/components/Navbar/index.js
+++ src/common/components/Navbar/index.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import { Menu } from 'element-react'
-import '../styles/navbar.css'
+import LoginDialog from './LoginDialog'
+import '../../styles/navbar.css'
 import 'element-theme-default'
 export default class Navbar extends React.Component {
+	constructor (props) {
+		super(props)
+		this.state = {
+			loginVisibility: false,
+		}
+	}
+
 	changePage (page) {
-		console.log(page)
+		if (page === '9') {
+			this.setState({
+				loginVisibility: true,
+			})
+		}
 	}
 
 	render () {
@@ -13,8 +25,9 @@ export default class Navbar extends React.Component {
 				<Menu theme='dark' defaultActive='1' className='el-menu-demo' mode='horizontal' onSelect={this.changePage.bind(this)}>
 					<Menu.Item index='1'>List</Menu.Item>
 					<Menu.Item index='2'>Map</Menu.Item>
-					<Menu.Item className='PullRight' index='3'>Login</Menu.Item>
+					<Menu.Item className='PullRight' index='9'>Login</Menu.Item>
 				</Menu>
+				<LoginDialog loginVisibility={this.state.loginVisibility} />
 			</div>
 		)
 	}
